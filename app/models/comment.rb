07e14ebbbf4ac5a9f86ab8b3329f8844e89d9b8c
Kevin Potter <kevinpfromnm@gmail.com>
2011-03-17 15:33:19 -0600

    Changes meant to be in initial commit.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,8 +10,6 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true, :counter_cache => true
   belongs_to :owner, :class_name => "User", :creator => true
 
-  has_many :content_reports, :as => :reportable
-
   named_scope :recent, lambda { |n| { :order => 'updated_at DESC', :limit => n } }
 
   set_default_order "created_at desc"
@@ -20,7 +18,7 @@ class Comment < ActiveRecord::Base
 
 
   def create_permitted?
-    owner_is? acting_user and acting_user.submit_permitted?
+    owner_is? acting_user
   end
 
   def update_permitted?
@@ -32,12 +30,7 @@ class Comment < ActiveRecord::Base
   end
 
   def view_permitted?(field)
-    acting_user.administrator? or content_reports.valid_reports.blank?
+    true
   end
 
-  # other methods
-  def shortened_body
-    return body.slice(0,100) + "..." if body.length > 100
-    body
-  end
 end
